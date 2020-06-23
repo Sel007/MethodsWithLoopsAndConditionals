17d9bf8cccfab658710e7f389cf6b5f6ad7a2b8b
Sel007 <selina.08225.1@gmail.com>
2020-06-23 19:03:13 -0400

    Add project files.

---
+++ MethodsWithLoopsAndConditionalsConsoleUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MethodsWithLoopsAndConditionalsConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("These are all numbers from 1,000 to -1,000");
+        }
+
+        public static int (string[] args)
+        {
+
+        }
+
+    }
+}
